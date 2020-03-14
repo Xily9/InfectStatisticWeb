49832eca5887d3b6bc6c6325faefc8d1b4054046
Xily9 <572089608@qq.com>
2020-03-14 15:55:26 +0800

    修复地图中空数据的省份显示NaN及不可点击的问题

--- src/views/index.vue
+++ src/views/index.vue
@@ -185,9 +185,46 @@ export default {
       } else {
         data = logData["augment"][Y + "-" + M + "-" + D];
       }
+      let keys = [
+        "安徽",
+        "北京",
+        "重庆",
+        "福建",
+        "甘肃",
+        "广东",
+        "广西",
+        "贵州",
+        "海南",
+        "河北",
+        "河南",
+        "黑龙江",
+        "湖北",
+        "湖南",
+        "吉林",
+        "江苏",
+        "江西",
+        "辽宁",
+        "内蒙古",
+        "宁夏",
+        "青海",
+        "山东",
+        "山西",
+        "陕西",
+        "上海",
+        "四川",
+        "天津",
+        "西藏",
+        "新疆",
+        "云南",
+        "浙江",
+        "台湾"
+      ];
       let arr = [];
-      for (var key in data) {
-        arr.push({ name: key, value: data[key]["infected"] });
+      for (var key of keys) {
+        arr.push({
+          name: key,
+          value: (data[key] ? data[key]["infected"] : 0)
+        });
       }
       this.map.series[0].data = arr;
     },
